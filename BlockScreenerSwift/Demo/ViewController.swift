44c12ff84fcb400669a4ac3bdc39f31a38813838
Bruce Chen <bruce@mitake.com.tw>
2023-11-22 17:48:26 +0800

    [New] Initial Project

---
+++ BlockScreenerSwift/Demo/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  BlockScreenerSwift
+//
+//  Created by KC on 2023/11/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
